d07ae7e9f9a1d78997656c9f00dff13052bfc162
sonjiwoon <thswldns77@naver.com>
2022-06-19 19:53:39 +0900

    second commit

--- datastructure/admin.py
+++ datastructure/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
